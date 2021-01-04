9880f6d3845ca3d9bb006b31ddd6fda9c356692b
j3ffh95 <j3ffh95@gmail.com>
2021-01-04 17:05:05 -0500

    Added api path to now playing movies

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const ApiUrl =
-  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1";
+// const ApiUrl = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1";
+const apiUrl =
+  "https://api.themoviedb.org/3/movie/now_playing?api_key=3fd2be6f0c70a2a598f084ddfb75487c&language=en-US&page=1";
 const imagePath = "https://image.tmdb.org/t/p/w1280";
 const searchApiPath =
   'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="';
@@ -8,3 +9,61 @@ const searchApiPath =
 const mainSection = document.querySelector("#main"),
   formElement = document.querySelector("#form"),
   seachElement = document.querySelector("#search");
+
+// Get movies from API
+getMovies(apiUrl);
+
+async function getMovies(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+
+  showMovies(data.results);
+}
+
+function showMovies(movies) {
+  main.innerHTML = "";
+
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie;
+
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+
+    movieEl.innerHTML = `
+            <img src="${imagePath + poster_path}" alt="${title}">
+            <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+          <h3>Overview</h3>
+          ${overview}
+        </div>
+        `;
+    main.appendChild(movieEl);
+  });
+}
+
+function getClassByRate(vote) {
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value;
+
+  if (searchTerm && searchTerm !== "") {
+    getMovies(searchApiPath + searchTerm);
+
+    search.value = "";
+  } else {
+    window.location.reload();
+  }
+});
