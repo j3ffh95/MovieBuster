df6c6f923d3b8671e20dccb3b5a757d4b5436355
j3ffh95 <j3ffh95@gmail.com>
2021-01-04 16:46:08 -0500

    get elements from the doc

--- main.js
+++ main.js
@@ -3,3 +3,8 @@ const ApiUrl =
 const imagePath = "https://image.tmdb.org/t/p/w1280";
 const searchApiPath =
   'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="';
+
+// Get elements from the doc
+const mainSection = document.querySelector("#main"),
+  formElement = document.querySelector("#form"),
+  seachElement = document.querySelector("#search");
